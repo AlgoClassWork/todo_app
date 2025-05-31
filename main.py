7d885393f129258384b6b3b48d886f3c93f10082
AlgoClassWork <kyxec.play@gmail.com>
2025-05-31 14:06:56 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 #pip install fastapi uvicorn[standart]
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from contextlib import asynccontextmanager
 
 from models import TodoRead
@@ -16,6 +18,7 @@ async def lifespan(app: FastAPI):
     print('Приложение остановилось')
 
 app = FastAPI(title='To Do list', lifespan=lifespan)
+app.mount('/static', StaticFiles(directory='static'), name='static_files')
 
 @app.get('/api/hello')
 async def hello():
@@ -24,5 +27,10 @@ async def hello():
 @app.get('/api/todos/',  summary='Получить все задачи')
 async def get_all_todos():
     return fake_todos_db
+
+@app.get('/', response_class=HTMLResponse)
+async def serve_index_html():
+    html_data = open('static/index.html', encoding='utf-8')
+    return HTMLResponse(content=html_data.read(), status_code=200)
 
 #uvicorn main:app --reload
