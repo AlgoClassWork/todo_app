8ad830b014e4e574d7a51ffb29d39f1b1ebe1d6c
AlgoClassWork <kyxec.play@gmail.com>
2025-05-31 13:34:01 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 #pip install fastapi uvicorn[standart]
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from typing import List
+
+from models import TodoRead
+
+fake_todos_db = [
+    TodoRead(id=1, title='Сделать дз', description='', done=False),
+    TodoRead(id=2, title='Помыть посуду', description='чисто', done=True)
+]
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,5 +21,9 @@ app = FastAPI(title='To Do list', lifespan=lifespan)
 @app.get('/api/hello')
 async def hello():
     return {'message': 'Fast Api работает'}
+
+@app.get('/api/todos/', response_model=List(TodoRead), summary='Получить все задачи')
+async def get_all_todos():
+    return fake_todos_db
 
 #uvicorn main:app --reload
