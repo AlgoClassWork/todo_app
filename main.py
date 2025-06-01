84124dfdb4147779b6c55b588518017588cbdeee
AlgoClassWork <kyxec.play@gmail.com>
2025-06-01 13:40:20 +0600

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from contextlib import asynccontextmanager
 from models import TodoRead
 
 fake_todos_db = [
-    TodoRead(id=1, title='Сделать дз', description='', done=False),
-    TodoRead(id=2, title='Помыть посуду', description='чисто', done=True)
+    TodoRead(id=1, title='Сделать дз', description='', done=True),
+    TodoRead(id=2, title='Помыть посуду', description='чисто', done=False)
 ]
+
+next_todo_id = len(fake_todos_db) + 1
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -27,6 +29,19 @@ async def hello():
 @app.get('/api/todos/',  summary='Получить все задачи')
 async def get_all_todos():
     return fake_todos_db
+
+@app.post('/api/todos/', response_model=TodoRead)
+async def create_new_todo(todo_data: TodoRead):
+    global next_todo_id
+    new_todo = TodoRead (
+        id = next_todo_id ,
+        title = todo_data.title ,
+        description = todo_data.description ,
+        done = False )
+    
+    fake_todos_db.append(new_todo) 
+    next_todo_id += 1
+    return new_todo
 
 @app.get('/', response_class=HTMLResponse)
 async def serve_index_html():
