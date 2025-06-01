ea95844164ca82b5f52fee0c718b2c017baf2d0d
AlgoClassWork <kyxec.play@gmail.com>
2025-06-01 13:50:06 +0600

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,5 +1,9 @@
 const activeTodoListULElement = document.getElementById('active-todo-list')
 const completedTodoListULElement = document.getElementById('completed-todo-list')
+const addTodoFormElement = document.getElementById('add-todo-form')
+const todoTitleInputElement = document.getElementById('todo-title-input')
+const todoDescriptionInputElement = document.getElementById('todo-description-input')
+
 
 async function fetchAndRenderTodos() {
     const response = await fetch('/api/todos/')
@@ -16,4 +20,11 @@ async function fetchAndRenderTodos() {
     })
 }
 
-window.onload = fetchAndRenderTodos
+async function addTodoForm(event) {
+    const title = todoTitleInputElement.value
+    alert(title)
+}
+
+
+addTodoFormElement.addEventListener('submit', addTodoForm)
+window.onload = fetchAndRenderTodos()
